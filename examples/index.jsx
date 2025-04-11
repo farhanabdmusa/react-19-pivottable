ede7f2fecc23d4c136ce46604926d804a2f5b0de
Farhan Abdurrahman Musa <farhan.musa@bps.go.id>
2025-04-11 16:05:49 +0700

    refactor: update rendering method to use createRoot for React 18 compatibility

--- examples/index.jsx
+++ examples/index.jsx
@@ -1,20 +1,21 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
-import { AppContainer } from 'react-hot-loader'
+import { createRoot } from 'react-dom/client'
 import App from './App'
 
-const render = Component => {
-  ReactDOM.render(
-    <AppContainer>
-      <Component />
-    </AppContainer>,
-    document.getElementById('app'),
-  )
+// Mount ke #app
+const container = document.getElementById('app')
+const root = createRoot(container)
+
+const render = (Component) => {
+  root.render(<Component />)
 }
 
 render(App)
 
-// Webpack Hot Module Replacement API
+// HMR (Hot Module Replacement)
 if (module.hot) {
-  module.hot.accept('./App', () => { render(App) })
+  module.hot.accept('./App', () => {
+    const NextApp = require('./App').default
+    render(NextApp)
+  })
 }
